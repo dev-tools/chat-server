4f0af281f2909639effe1e61ee0c8c9e8d7a43b4
Igor Projoga <igor.pro@speedflow.com>
2013-10-28 21:58:18 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-chat-server
+IRC server
 ===========
---
+++ ircserver.py
@@ -0,0 +1,186 @@
+from twisted.internet.protocol import ServerFactory
+from twisted.internet import reactor
+from twisted.words.protocols.irc import IRC
+from twisted.words.protocols.irc import symbolic_to_numeric as stn
+from twisted.python import log
+import sys
+import socket
+
+log.startLogging(sys.stdout)
+
+auth = {'login1': 'password1', 'login': 'password'}
+
+
+class IRCServer(IRC):
+    hostname = '192.168.0.104'
+    channels = []
+
+    def connectionMade(self):
+        log.msg('client connected')
+        if self.hostname is None:
+            self.hostname = socket.getfqdn()
+
+    def get_host(self):
+        return self.transport.getPeer().host
+
+    def get_channel(self, name):
+        for ch in self.channels:
+            if ch.name == name:
+                return ch
+
+    def dataReceived(self, data):
+        log.msg('GET: {0}'.format(data))
+        IRC.dataReceived(self, data)
+
+    def sendMessage(self, command, *parameter_list, **prefix):
+        if not command:
+            raise ValueError("IRC message requires a command.")
+
+        if ' ' in command or command[0] == ':':
+            raise ValueError("Somebody screwed up, 'cuz this doesn't look like a command to me: {0}".format(command))
+
+        line = ' '.join([command] + list([str(i) for i in parameter_list]))
+        if 'prefix' in prefix:
+            line = ":{0} {1}".format(prefix['prefix'], line)
+        else:
+            line = ':' + self.hostname + ' ' + line
+        log.msg('SEND: {0}'.format(line))
+        self.sendLine(line)
+
+        if len(parameter_list) > 15:
+            log.msg("Message has %d parameters (RFC allows 15):\n{0}".format(len(parameter_list), line))
+
+    def _authparams(self, *args, **kwargs):
+        for key in kwargs.keys():
+            setattr(self, key, kwargs[key])
+        if getattr(self, 'password', None) and getattr(self, 'username', None):
+            if self.username in auth and auth[self.username] == self.password or True:
+                self.sendMessage('NOTICE', 'AUTH :*** You connected on 6667 port')
+                #TODO
+                self.sendMessage(stn['RPL_WELCOME'], self.nickname, ':Welcome to Dev Team IRC')
+                self.sendMessage(stn['RPL_YOURHOST'], self.nickname, ':Welcome to Dev Team IRC')
+                self.sendMessage(stn['RPL_CREATED'], self.nickname, ':Welcome to Dev Team IRC')
+                self.sendMessage(stn['RPL_MYINFO'], self.nickname, ':Welcome to Dev Team IRC')
+                self.sendMessage(stn['RPL_ISUPPORT'], self.nickname, ':Welcome to Dev Team IRC')
+                return None
+            #TODO send ERR_PASSWDMISMATCH
+            self.transport.loseConnection()
+
+    def irc_unknown(self, prefix, command, params):
+        log.msg("{0}, {1}, {2}, IRC UNKNOWN".format(prefix, command, params))
+
+    def irc_USER(self, prefix, params):
+        if len(params) == 4:
+            self._authparams(username=params[0], mode=params[1],
+                             unused=params[2], realname=params[3])
+
+    def irc_NICK(self, prefix, params):
+        self.nickname = params[0]
+
+    def irc_PASS(self, prefix, params):
+        if len(params):
+            self._authparams(password=params[0])
+
+    def irc_PING(self, prefix, params):
+        self.sendMessage('PONG')
+
+    def irc_NOTICE(self, prefix, params):
+        pass
+
+    def irc_JOIN(self, prefix, params):
+        ch = self.factory.get_channel(params[0])
+        ch.userjoin(self)
+        self.channels.append(ch)
+        self.sendMessage('JOIN', ':{0}'.format(ch.name), **{'prefix': self.nickname + '!~' + self.username + '@' + self.get_host()})
+        self.sendMessage(stn['RPL_NAMREPLY'], self.nickname, '=', ch.name, ':{0}'.format(' '.join([i.nickname for i in ch.users])))
+        self.sendMessage(stn['RPL_ENDOFNAMES'], self.nickname, ch.name, ':{0}'.format('End of /NAMES list.'))
+        self.sendMessage(stn['RPL_TOPIC'], self.nickname, ch.name, ':{0}'.format('Welcome to channel'))
+
+
+    def irc_QUIT(self, prefix, params):
+        log.msg('client is logging off')
+        self.transport.loseConnection()
+        for ch in self.channels:
+            for i in ch.users:
+                if i == self:
+                    ch.users.remove(i)
+            #ch.senduserlist()
+
+    def irc_LIST(self, prefix, params):
+        self.sendMessage(stn['RPL_LISTSTART'], self.nickname, 'Channel :Users  Name')
+        for ch in self.factory.channel_list:
+            self.sendMessage(stn['RPL_LIST'], self.nickname, ch.name,
+                             ch.count(), ':[+nt] ' + ch.topic)
+        self.sendMessage(stn['RPL_LISTEND'], self.nickname, ':END of /LIST')
+
+    def irc_PRIVMSG(self, prefix, params):
+        recipient, msg = params[0], params[1]
+        if recipient[0] == '#':
+            ch = self.get_channel(recipient)
+            ch.sendmsg(self, msg)
+            return None
+        else:
+            for ch in self.channels:
+                user = ch.getuser(recipient)
+                user.sendMessage('PRIVMSG', user.nickname, ':{0}'.format(msg), prefix=self.nickname+'!'+self.username+'@'+self.get_host())
+                return None
+
+    def irc_WHO(self, prefix, params):
+        ch = self.get_channel(params[0])
+        for user in ch.users:
+            self.sendMessage(stn['RPL_WHOREPLY'], self.nickname, ch.name, user.nickname)
+        self.sendMessage(stn['RPL_ENDOFWHO'], self.nickname, ch.name, ':End of /WHO list.')
+
+
+class IRCChannel(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.topic = 'channel topic'
+        self.users = []
+
+    def count(self):
+        return len(self.users)
+
+    def userjoin(self, user):
+        if not user in self.users:
+            self.users.append(user)
+        self.senduserlist()
+
+    def senduserlist(self):
+        users = [i.nickname for i in self.users]
+        for user in self.users:
+            user.sendMessage(stn['RPL_NAMREPLY'], user.nickname, '=', self.name, ':{0}'.format(' '.join(users)))
+            user.sendMessage(stn['RPL_ENDOFNAMES'], user.nickname, self.name, ':{0}'.format('End of /NAMES list.'))
+
+    def sendmsg(self, sender, msg):
+        for u in self.users:
+            if u != sender:
+                u.sendMessage('PRIVMSG', self.name, ':{0}'.format(msg), prefix=sender.nickname+'!'+sender.username+'@'+sender.get_host())
+
+    def getuser(self, nickname):
+        for u in self.users:
+            if u.nickname.lower() == nickname.lower():
+                return u
+
+
+class IRCServerFactory(ServerFactory):
+    channel_list = []
+
+    def get_channel(self, name):
+        for ch in self.channel_list:
+            if ch.name == name:
+                return ch
+        ch = IRCChannel(name)
+        self.channel_list.append(ch)
+        return ch
+
+    def buildProtocol(self, addr):
+        p = IRCServer()
+        p.factory = self
+        return p
+
+
+factory = IRCServerFactory()
+reactor.listenTCP(6667, factory)
+reactor.run()
